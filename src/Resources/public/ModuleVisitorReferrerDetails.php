236fe5e98d72730242339ee2a8e40f48e8276a99
Glen Langer <contao.modul@glen-langer.de>
2017-04-18 18:06:08 +0200

    Base Prad und CSS Klassen angepasst

--- src/Resources/public/ModuleVisitorReferrerDetails.php
+++ src/Resources/public/ModuleVisitorReferrerDetails.php
@@ -69,12 +69,13 @@ class ModuleVisitorReferrerDetails extends \Backend // Backend bringt DB mit
    	        echo "<html><body>".$GLOBALS['TL_LANG']['tl_visitors_referrer']['no_referrer']."</body></html>";
             return ;
 	    }
-	    
-	    echo 'echo <!DOCTYPE html>
+	    $base = \Environment::get('base'); // in contao4 hier: http://domain.de/bundles/bugbustervisitors/
+	    $base = explode('bundles/bugbustervisitors', $base);
+	    echo '<!DOCTYPE html>
 <html lang="de">	    
 <head>
 <meta charset="utf-8">
-<base href="'.\Environment::get('base').'"></base>
+<base href="'.$base[0].'"></base>
 <meta name="generator" content="Contao Open Source CMS">
 <title>Contao Open Source CMS '.VERSION.'</title>
 <link rel="stylesheet" type="text/css" href="system/themes/'.$this->getTheme().'/basic.css" media="screen" />
@@ -87,15 +88,13 @@ echo '
 </head>
 <body id="top">
 <div id="main">
-	<br>
 	<h1 class="main_headline">'.$GLOBALS['TL_LANG']['tl_visitors_referrer']['details_for'].': '.\Idna::decode(str_rot13($this->Input->get('tl_referrer',true))).'</h1>
-	<br><br>
-	<div class="tl_formbody_edit">
-		<table cellpadding="0" cellspacing="0" summary="Table lists records" class="mod_visitors_be_table">
+	<div class="tl_listing_container list_view">
+		<table cellpadding="0" cellspacing="0" summary="Table lists records" class="mod_visitors_be_table_version">
 		<tbody>
 			<tr>
 				<td style="padding-left: 2px;" class="tl_folder_tlist">'.$GLOBALS['TL_LANG']['tl_visitors_referrer']['visitor_referrer'].'</td>
-				<td style="width: 120px; padding-left: 2px;" class="tl_folder_tlist">'.$GLOBALS['TL_LANG']['tl_visitors_referrer']['visitor_referrer_last_seen'].'</td>
+				<td style="width: 145px; padding-left: 2px;" class="tl_folder_tlist">'.$GLOBALS['TL_LANG']['tl_visitors_referrer']['visitor_referrer_last_seen'].'</td>
 				<td style="width: 80px; padding-left: 2px; text-align: center;" class="tl_folder_tlist">'.$GLOBALS['TL_LANG']['tl_visitors_referrer']['number'].'</td>
 			</tr>';
 		/*$objDetails = \Database::getInstance()->prepare("SELECT `visitors_referrer_full`, count(id) as ANZ"
