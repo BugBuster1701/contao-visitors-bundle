1181de4afa689e27417940284a917b3ac2804013
Glen Langer <contao.modul@glen-langer.de>
2018-12-20 21:51:13 +0100

    Fixed #55, Update language files

--- src/Resources/contao/languages/bg_BG/tl_visitors_category.php
+++ src/Resources/contao/languages/bg_BG/tl_visitors_category.php
@@ -8,35 +8,37 @@
  * @link https://www.transifex.com/signup/
  * @link https://www.transifex.com/projects/p/contao-visitors-bundle/language/bg_BG/
  *
- * last-updated: 2017-10-21T01:05:55+02:00
+ * last-updated: 2017-12-18T23:54:17+01:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_visitors_category']['copy']['0']                    = 'Направи копие на категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['copy']['1']                    = 'Направи копие на категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['delete']['0']                  = 'Изтрий категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['delete']['1']                  = 'Изтрий категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['deleteConfirm']                = 'Изстривайки дадена категория - ще изтриете и всички данни от броенето. Сигурни ли сте, че искате да изтриете категория с ID %s?';
-$GLOBALS['TL_LANG']['tl_visitors_category']['edit']['0']                    = 'Редактирай категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['edit']['1']                    = 'Редактирай категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['editheader']['0']              = 'Редактирай настройките на категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['editheader']['1']              = 'Редактирай настройките за категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['new']['0']                     = 'Създай категория';
-$GLOBALS['TL_LANG']['tl_visitors_category']['new']['1']                     = 'Създаване на нова категория';
-$GLOBALS['TL_LANG']['tl_visitors_category']['protected_stat_legend']        = 'Защита на достъпа до статистиките';
-$GLOBALS['TL_LANG']['tl_visitors_category']['show']['0']                    = 'Подробности за категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['show']['1']                    = 'Показване на подробности за категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['stat']['0']                    = 'Статистики за категорията';
-$GLOBALS['TL_LANG']['tl_visitors_category']['stat']['1']                    = 'Показване на статистиките за категория с ID %s';
-$GLOBALS['TL_LANG']['tl_visitors_category']['title']['0']                   = 'Заглавие';
-$GLOBALS['TL_LANG']['tl_visitors_category']['title']['1']                   = 'Моля, определете заглавие за категорията.';
-$GLOBALS['TL_LANG']['tl_visitors_category']['title_legend']                 = 'Заглавие';
-$GLOBALS['TL_LANG']['tl_visitors_category']['tstamp']['0']                  = 'Последна ревизия';
-$GLOBALS['TL_LANG']['tl_visitors_category']['tstamp']['1']                  = 'Дата и час на последната ревизия';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_admins']['0']    = 'Администратори';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_admins']['1']    = 'Администраторите имат неограничен достъп до всички статистики за посещения.';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_groups']['0']    = 'Потребителски групи с достъп';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_groups']['1']    = 'Тук можете да изберете потребителските групи, които ще могат да виждат статистиките за посетителите за тази категория.';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_protected']['0'] = 'Защита на статистиките';
-$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_protected']['1'] = 'Показване на статистиките за посещения за тази категория само на определени групи от потребители.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['copy']['0']                      = 'Направи копие на категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['copy']['1']                      = 'Направи копие на категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['delete']['0']                    = 'Изтрий категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['delete']['1']                    = 'Изтрий категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['deleteConfirm']                  = 'Изстривайки дадена категория - ще изтриете и всички данни от броенето. Сигурни ли сте, че искате да изтриете категория с ID %s?';
+$GLOBALS['TL_LANG']['tl_visitors_category']['edit']['0']                      = 'Редактирай категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['edit']['1']                      = 'Редактирай категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['editheader']['0']                = 'Редактирай настройките на категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['editheader']['1']                = 'Редактирай настройките за категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['new']['0']                       = 'Създай категория';
+$GLOBALS['TL_LANG']['tl_visitors_category']['new']['1']                       = 'Създаване на нова категория';
+$GLOBALS['TL_LANG']['tl_visitors_category']['protected_stat_legend']          = 'Защита на достъпа до статистиките';
+$GLOBALS['TL_LANG']['tl_visitors_category']['show']['0']                      = 'Подробности за категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['show']['1']                      = 'Показване на подробности за категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['stat']['0']                      = 'Статистики за категорията';
+$GLOBALS['TL_LANG']['tl_visitors_category']['stat']['1']                      = 'Показване на статистиките за категория с ID %s';
+$GLOBALS['TL_LANG']['tl_visitors_category']['title']['0']                     = 'Заглавие';
+$GLOBALS['TL_LANG']['tl_visitors_category']['title']['1']                     = 'Моля, определете заглавие за категорията.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['title_legend']                   = 'Заглавие';
+$GLOBALS['TL_LANG']['tl_visitors_category']['tstamp']['0']                    = 'Последна ревизия';
+$GLOBALS['TL_LANG']['tl_visitors_category']['tstamp']['1']                    = 'Дата и час на последната ревизия';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_admins']['0']      = 'Администратори';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_admins']['1']      = 'Администраторите имат неограничен достъп до всички статистики за посещения.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_groups']['0']      = 'Потребителски групи с достъп';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_groups']['1']      = 'Тук можете да изберете потребителските групи, които ще могат да виждат статистиките за посетителите за тази категория.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_protected']['0']   = 'Защита на статистиките';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_stat_protected']['1']   = 'Показване на статистиките за посещения за тази категория само на определени групи от потребители.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_statreset_admins']['0'] = 'Администратори';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_statreset_admins']['1'] = 'Администраторите имат неограничен достъп до всички статистики за посещения.';
+$GLOBALS['TL_LANG']['tl_visitors_category']['visitors_statreset_groups']['0'] = 'Потребителски групи с достъп';
 
