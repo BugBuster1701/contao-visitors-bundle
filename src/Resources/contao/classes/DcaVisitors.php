3e1b0276941e04d608e1375dc9b9da2fc0503d11
Glen Langer <contao.modul@glen-langer.de>
2019-07-21 15:14:45 +0200

    Update DcaVisitors.php
    Published/Unpublished Anzeige korrigiert

--- src/Resources/contao/classes/DcaVisitors.php
+++ src/Resources/contao/classes/DcaVisitors.php
@@ -47,7 +47,7 @@ class DcaVisitors extends \Backend
         } else {
             $startdate = date($GLOBALS['TL_CONFIG']['dateFormat'], $arrRow['visitors_startdate']);
         }
-        $output = '<div class="cte_type ' . $key . '"><strong>' . $arrRow['visitors_name'] . '</strong></div>';
+        $output = '<div class="cte_type ' . $key . '"><span class="tl_label">' . $arrRow['visitors_name'] . '</span></div>';
         $output.= '<div>'.$GLOBALS['TL_LANG']['tl_visitors']['visitors_startdate'][0].': ' . $startdate . '</div>';
         //Debug $output.= '<div>'.print_r($arrRow,true).'</div>';
         return $output;
