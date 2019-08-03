740b102eb760a1e15109b0373235edee85ca6074
Glen Langer <contao.modul@glen-langer.de>
2019-08-03 22:59:58 +0200

    Fixed #62, Backend: CSS Klassen visitors_stat_yes, visitors_stat_no

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Extension for Contao Open Source CMS, Copyright (C) 2005-2017 Leo Feyer
+ * Extension for Contao Open Source CMS, Copyright (C) 2005-2019 Leo Feyer
  * 
  * Modul Visitors Config File
  *
- * @copyright  Glen Langer 2009..2017 <http://contao.ninja>
+ * @copyright  Glen Langer 2009..2019 <http://contao.ninja>
  * @author     Glen Langer (BugBuster)
  * @licence    LGPL
  * @filesource
@@ -13,13 +13,13 @@
  */
 
 \define('VISITORS_VERSION', '1.5');
-\define('VISITORS_BUILD', '3');
+\define('VISITORS_BUILD', '4');
 
 /**
  * Backend css version number are added to style sheets files to make
  * the web browser reload those resources after a Visitors update.
  */
-\define('VISITORS_BE_CSS', '1.1.0');
+\define('VISITORS_BE_CSS', '1.2.0');
 
 /**
  * -------------------------------------------------------------------------
