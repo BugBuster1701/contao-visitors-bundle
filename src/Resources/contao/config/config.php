20ae3a7d7b6ce03af7641237f3ca24899cef0aab
Glen Langer <contao.modul@glen-langer.de>
2019-08-31 22:19:05 +0200

    Versionnumber

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,8 +12,8 @@
  * @see	       https://github.com/BugBuster1701/contao-visitors-bundle
  */
 
-\define('VISITORS_VERSION', '1.5');
-\define('VISITORS_BUILD', '4');
+\define('VISITORS_VERSION', '1.6');
+\define('VISITORS_BUILD', '0');
 
 /**
  * Backend css version number are added to style sheets files to make
