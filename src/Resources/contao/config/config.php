64c463f2759fb159894fb96a3d2033a7a4c48b21
Glen Langer <contao.modul@glen-langer.de>
2021-02-28 02:31:02 +0100

    #93, Template Anpassungen

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -19,7 +19,7 @@
  * Backend css version number are added to style sheets files to make
  * the web browser reload those resources after a Visitors update.
  */
-\define('VISITORS_BE_CSS', '1.2.0');
+\define('VISITORS_BE_CSS', '1.2.1');
 
 /**
  * -------------------------------------------------------------------------
