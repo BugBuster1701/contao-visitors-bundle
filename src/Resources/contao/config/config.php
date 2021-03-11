917fa503b83939f5d5f72df78726de09c76d440b
Glen Langer <contao.modul@glen-langer.de>
2021-03-11 01:47:52 +0100

    Fixed #97, PHP 8: Warning: Undefined array key "export_button_title"
    for Contao 4.9, for Contao 4.10+ follows

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,7 +13,7 @@
  */
 
 \define('VISITORS_VERSION', '1.6');
-\define('VISITORS_BUILD', '7');
+\define('VISITORS_BUILD', '8');
 
 /**
  * Backend css version number are added to style sheets files to make
