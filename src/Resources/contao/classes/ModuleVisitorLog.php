a95cb57f00c42dd2e2178c4fb3c392d5ab89e4c2
Glen Langer <contao.modul@glen-langer.de>
2021-08-14 17:32:17 +0200

    Fixed #110, Warning: Undefined array key "visitors"

--- src/Resources/contao/classes/ModuleVisitorLog.php
+++ src/Resources/contao/classes/ModuleVisitorLog.php
@@ -31,15 +31,21 @@ class ModuleVisitorLog
      */
     public static function writeLog($method, $line, $value)
     {
+        $GLOBALS['visitors']['debug']['tag']                   = $GLOBALS['visitors']['debug']['tag'] ?? false;
+        $GLOBALS['visitors']['debug']['checks']                = $GLOBALS['visitors']['debug']['checks'] ?? false;
+        $GLOBALS['visitors']['debug']['referrer']              = $GLOBALS['visitors']['debug']['referrer'] ?? false;
+        $GLOBALS['visitors']['debug']['searchengine']          = $GLOBALS['visitors']['debug']['searchengine'] ?? false;
+        $GLOBALS['visitors']['debug']['screenresolutioncount'] = $GLOBALS['visitors']['debug']['screenresolutioncount'] ?? false;
+
         if ($method == '## START ##') 
         {
             if (!isset($GLOBALS['visitors']['debug']['first'])) 
             {
-                if ((bool) ($GLOBALS['visitors']['debug']['tag'] ?? false)          ||
-                    (bool) ($GLOBALS['visitors']['debug']['checks'] ?? false)       ||
-                    (bool) ($GLOBALS['visitors']['debug']['referrer'] ?? false)     ||
-                    (bool) ($GLOBALS['visitors']['debug']['searchengine'] ?? false) ||
-                    (bool) ($GLOBALS['visitors']['debug']['screenresolutioncount'] ?? false)
+                if ((bool) $GLOBALS['visitors']['debug']['tag']          ||
+                    (bool) $GLOBALS['visitors']['debug']['checks']       ||
+                    (bool) $GLOBALS['visitors']['debug']['referrer']     ||
+                    (bool) $GLOBALS['visitors']['debug']['searchengine'] ||
+                    (bool) $GLOBALS['visitors']['debug']['screenresolutioncount']
                    )
                 {
                     $arrUniqid = StringUtil::trimsplit('.', uniqid('c0n7a0', true));
