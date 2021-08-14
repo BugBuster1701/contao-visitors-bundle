c7ef663b95cb45942c9b409c1684e278d24c6f4f
Glen Langer <contao.modul@glen-langer.de>
2021-08-14 17:00:58 +0200

    Versionsnummern angepasst

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,7 +13,7 @@
  */
 
 \define('VISITORS_VERSION', '1.6');
-\define('VISITORS_BUILD', '11');
+\define('VISITORS_BUILD', '12');
 
 /**
  * Backend css version number are added to style sheets files to make
