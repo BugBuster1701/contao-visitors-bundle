610dcce1e40e5a4b4fe84b23d9aa303209921f43
Glen Langer <contao.modul@glen-langer.de>
2022-11-22 22:48:02 +0100

    VISITORS_BUILD Nummer angepasst

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,7 +13,7 @@
  */
 
 \define('VISITORS_VERSION', '1.8');
-\define('VISITORS_BUILD', '1');
+\define('VISITORS_BUILD', '2');
 
 /**
  * Backend css version number are added to style sheets files to make
