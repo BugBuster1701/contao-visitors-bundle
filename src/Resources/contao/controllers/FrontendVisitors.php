7581b1619bb5039d30da979f1903a4a5d3f0a3c4
Glen Langer <contao.modul@glen-langer.de>
2023-07-30 18:11:00 +0200

    #136, FE_USER_LOGGED_IN, BE_USER_LOGGED_IN entfernt

--- src/Resources/contao/controllers/FrontendVisitors.php
+++ src/Resources/contao/controllers/FrontendVisitors.php
@@ -34,17 +34,6 @@ class FrontendVisitors extends \Contao\Frontend
 	{
 		parent::__construct();
 
-		// See #4099
-		if (!\defined('BE_USER_LOGGED_IN'))
-		{
-			\define('BE_USER_LOGGED_IN', false);
-		}
-
-		if (!\defined('FE_USER_LOGGED_IN'))
-		{
-			\define('FE_USER_LOGGED_IN', false);
-		}
-
 		\Contao\System::loadLanguageFile('tl_visitors');
 	}
 
