9b6b08f898b1c87f8c63581c2d21d41cdc3fd682
Glen Langer <contao.modul@glen-langer.de>
2023-09-14 01:13:38 +0200

    #151, phpdoc in ecs configs

--- tools/ecs/config/legacy.php
+++ tools/ecs/config/legacy.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of a BugBuster Contao Bundle.
+ *
+ * @copyright  Glen Langer 2023 <http://contao.ninja>
+ * @author     Glen Langer (BugBuster)
+ * @package    Contao Visitors Bundle
+ * @link       https://github.com/BugBuster1701/contao-visitors-bundle
+ *
+ * @license    LGPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
 use Contao\EasyCodingStandard\Fixer\MultiLineLambdaFunctionArgumentsFixer;
 use Contao\EasyCodingStandard\Fixer\TypeHintOrderFixer;
 use Contao\EasyCodingStandard\Sniffs\UseSprintfInExceptionsSniff;
